678a4a82d9e9d7c887b1519b28dd9b342c9d10dc
ghaid999 <gaid.909@gmail.com>
2026-02-28 22:05:44 +0300

    Improve documentation for print_sign function
    Update function documentation for print_sign.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_sign - prints the sign of a number
+ * @n: the number to check
  *
- * Return: Always 0.
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
  */
 int print_sign(int n)
 {
