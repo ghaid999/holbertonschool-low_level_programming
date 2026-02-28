b89f7a76ccab04a80b0d97667da17b4670143f97
ghaid999 <gaid.909@gmail.com>
2026-02-28 23:39:59 +0300

    Update 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,13 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    /* Handle negative numbers */
-    if (n < 0)
-        n = -n;
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-    last_digit = n % 10;
+	_putchar('0' + last_digit);
 
-    _putchar('0' + last_digit);
-
-    return (last_digit);
+	return (last_digit);
 }
