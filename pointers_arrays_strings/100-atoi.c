89360c4fa731a9a5a8cec14fcf0f2ddb61c21928
ghaid999 <gaid.909@gmail.com>
2026-03-07 21:21:35 +0300

    Refactor _atoi for better digit and sign handling
    Refactor _atoi function to improve digit handling and sign processing.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,20 +10,28 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int result = 0;
+	int res = 0;
+	int found_digit = 0;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			res = (res * 10) - (s[i] - '0');
+		}
+		else if (found_digit)
+		{
+			break;
+		}
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+	if (sign > 0)
+		return (-res);
 
-	return (sign * result);
+	return (res);
 }
