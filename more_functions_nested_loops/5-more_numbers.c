6acd321d978f4801c019e8250b375cafc2a733a3
ghaid999 <gaid.909@gmail.com>
2026-03-07 06:29:10 +0300

    Remove unnecessary else block in number printing

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,10 +17,7 @@ void more_numbers(void)
             {
                 _putchar((n / 10) + '0');
             }
-            else
-            {
                 _putchar((n % 10) + '0');
-            }
         }
         _putchar('\n');
     }
