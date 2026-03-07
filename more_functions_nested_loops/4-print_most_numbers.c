9fabecfbb4016c9e9775f914fcc20f4e66820d8a
ghaid999 <gaid.909@gmail.com>
2026-03-07 05:54:26 +0300

    Rename function from print_most_numbers to print_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
  *
  * Return: void
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int c;
 
 	for (c = 0; c <= 9; c++)
 	{
-if (c == 2 || c == 4)
-    continue; 
-		_putchar(c + '0');
+		if (c == 2 || c == 4)
+			continue;        
+		_putchar(c + '0');    
 	}
 	_putchar('\n');
 }
