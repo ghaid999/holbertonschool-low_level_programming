d3660f13464c30435900ee56916ba7d23ff17687
ghaid999 <gaid.909@gmail.com>
2026-03-07 07:02:04 +0300

    Change documentation for print_square function
    Updated function documentation to reflect the new functionality.

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square n times.
+ * @n: the number of times the # should be printed
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('#');
+		}
+     _putchar('\n');
+	}
+	
+}
