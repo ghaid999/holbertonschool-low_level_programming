cf24ff982e4ede3f4f153eaf75aae9572a5e9576
ghaid999 <gaid.909@gmail.com>
2026-03-07 07:03:33 +0300

    Fix parameter name in print_square function
    Updated function parameter from 'n' to 'size' for clarity.

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints a square n times.
- * @n: the number of times the # should be printed
+ * @size: the number of times the # should be printed
  *
  * Return: void
  */
@@ -11,9 +11,9 @@ void print_square(int size)
 {
 	int i;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
 			_putchar('#');
 		}
