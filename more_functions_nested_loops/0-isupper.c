74db32c7871fa0297ed98de1910430ad01c520ba
ghaid999 <gaid.909@gmail.com>
2026-03-07 01:01:15 +0000

    Add function _isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+
+int _isupper(int c){
+
+  if ( c >= 'A' && c <= 'Z')
+   return(1);
+
+  else
+   return(0);
+
+}
+
+i
+
+
