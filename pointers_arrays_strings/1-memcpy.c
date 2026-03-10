fc5b08e0f83e24444ed87ad13341faf786af370a
ghaid999 <gaid.909@gmail.com>
2026-03-10 21:32:28 +0300

    Fix _memcpy to copy memory area from src to dest
    Updated _memcpy function to copy bytes from source to destination correctly.

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer to the memory area to be filled
- * @b: the constant byte used to fill the memory
- * @n: number of bytes to fill
+ * _memcpy - copies memory area
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: number of bytes to copy
  *
- * Return: pointer to the memory area s
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -15,7 +15,7 @@ unsigned int i;
 
 for (i = 0; i < n; i++)
 {
-dest [i] = *src;
+dest[i] = src[i];
 }
 
 return (dest);
