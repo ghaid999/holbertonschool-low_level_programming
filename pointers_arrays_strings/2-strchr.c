aa7d95f0b41caaf4642d8d1cea2a951e6af4326a
ghaid999 <gaid.909@gmail.com>
2026-03-10 21:39:00 +0300

    Add _strchr function to find character in string
    Implement strchr function to locate character in string.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+
+for (i = 0; i < ; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
