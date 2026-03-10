ee8ebd4b741767394835848d157aac93d3285e92
ghaid999 <gaid.909@gmail.com>
2026-03-10 21:50:25 +0300

    Fix _strchr function implementation and comments
    Refactor _strchr function to improve logic and comments.

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- * _strchr - Returns a pointer to the first occurrence of the character c
- * @s: the string to seach in
- * @c: the character to found
- * 
+ * _strchr - returns a pointer to the first occurrence of a character
+ * @s: string to search in
+ * @c: character to search for
  *
- * Return: pointer to the fist occurrence of the character or NULL if not found
+ * Return: pointer to the first occurrence of c or NULL
  */
 
 char *_strchr(char *s, char c)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i] != '\0')
-{
-  if (s[i] == c)
-    s=s[i];
-  return (s);
-i++;
-}
-  if (s[i] == '\0')
-    return(NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
 
+	if (c == '\0')
+		return (&s[i]);
 
-
+	return (NULL);
 }
