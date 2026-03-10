84a164b50aed9d00b70a19394e4e63b47b1da949
ghaid999 <gaid.909@gmail.com>
2026-03-10 21:27:00 +0300

    Create 1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte used to fill the memory
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest [i] = src;
+}
+
+return (dest);
+}
