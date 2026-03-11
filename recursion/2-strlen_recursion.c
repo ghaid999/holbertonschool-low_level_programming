24054faba7b5fa4cd8b5ada587ffe1d633315806
ghaid999 <gaid.909@gmail.com>
2026-03-11 21:52:27 +0300

    Create 2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate length 
+ */
+
+int _strlen_recursion(char *s)
+{
+int sum = 0;
+	if (*s == '\0')
+		return(0);
+
+	return sum = sum _print_rev_recursion(s + 1);
+	
+}
