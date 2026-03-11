1767be33b05f1bd07781ed808043b4cd936d5b56
ghaid999 <gaid.909@gmail.com>
2026-03-11 21:28:04 +0300

    Add _puts_recursion function for string printing
    Implement a recursive function to print a string followed by a new line.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print the string
+ * @s : the string to print
+ * Return: the string folliwd by a new line.
+ */
+
+
+void _puts_recursion(char *s){
+
+if (s == '\0')
+  return ('\n');
+
+  return _putchar (s) + void _puts_recursion(s + 1);
+
+}
