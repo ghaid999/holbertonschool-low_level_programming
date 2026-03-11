5e795975789ae69707833c2d80a9ee0971c6a1a9
ghaid999 <gaid.909@gmail.com>
2026-03-11 23:09:08 +0300

    Fix _strlen_recursion implementation and comments

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,17 +2,15 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate length 
- * @sum : contain the length
+ * @s: string to calculate length
+ *
+ * Return: length of the string
  */
 
 int _strlen_recursion(char *s)
 {
-int sum;
 	if (*s == '\0')
-		return(0);
+		return (0);
 
-	 sum = sum++ +  _print_rev_recursion(s + 1);
-	return sum;
-	
+	return (1 + _strlen_recursion(s + 1));
 }
