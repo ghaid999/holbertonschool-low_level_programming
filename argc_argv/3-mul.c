5265381eefb3b81c782eed4bd79b7c10137d6e1a
ghaid999 <gaid.909@gmail.com>
2026-03-14 21:26:39 +0300

    Create 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+int mul=1;
+if (argc == 0)
+printf("Error");
+else
+for (i = 1 ; i < 3 ; i++)
+mul = mul * argv[i];
+
+printf("%i\n", mul);
+return (0);
+}
