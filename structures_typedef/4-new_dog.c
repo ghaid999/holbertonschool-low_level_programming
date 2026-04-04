9c8c1a389220a99e3beab9f49138a8104a4b1caa
ghaid999 <gaid.909@gmail.com>
2026-04-04 17:29:09 +0300

    Refactor new_dog function for name and owner copying

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,23 +13,48 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-char *name_copy, *owner_copy;
+char *name_copy = NULL;
+char *owner_copy = NULL;
+size_t len;
 
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
 
-name_copy = (name != NULL) ? strdup(name) : NULL;
-owner_copy = (owner != NULL) ? strdup(owner) : NULL;
-
-if ((name != NULL && name_copy == NULL) || (owner != NULL && owner_copy == NULL))
+/* copy name */
+if (name != NULL)
 {
-free(name_copy);
-free(owner_copy);
+len = 0;
+while (name[len] != '\0')
+len++;
+name_copy = malloc(len + 1);
+if (name_copy == NULL)
+{
 free(d);
 return (NULL);
 }
+for (size_t i = 0; i <= len; i++)
+name_copy[i] = name[i];
+}
 
+/* copy owner */
+if (owner != NULL)
+{
+len = 0;
+while (owner[len] != '\0')
+len++;
+owner_copy = malloc(len + 1);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(d);
+return (NULL);
+}
+for (size_t i = 0; i <= len; i++)
+owner_copy[i] = owner[i];
+}
+
+/* assign values */
 d->name = name_copy;
 d->age = age;
 d->owner = owner_copy;
