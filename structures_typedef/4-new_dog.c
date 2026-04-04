31a19ed55a561c7ab3ace274d6c479e7641210b9
ghaid999 <gaid.909@gmail.com>
2026-04-04 17:34:34 +0300

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,54 +1,56 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/* helper: duplicate string manually */
+char *str_copy(char *str)
+{
+char *copy;
+unsigned int len = 0, i;
+
+if (str == NULL)
+return (NULL);
+
+while (str[len] != '\0')
+len++;
+
+copy = malloc(len + 1);
+if (copy == NULL)
+return (NULL);
+
+for (i = 0; i <= len; i++)
+copy[i] = str[i];
+
+return (copy);
+}
+
 /**
 * new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
+*
 * Return: pointer to the newly created dog, or NULL on failure
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-char *name_copy = NULL;
-char *owner_copy = NULL;
-unsigned int len;
-unsigned int i;
+
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
-if (name != NULL)
+
+d->name = str_copy(name);
+d->owner = str_copy(owner);
+
+if ((name != NULL && d->name == NULL) || (owner != NULL && d->owner == NULL))
 {
-len = 0;
-while (name[len] != '\0')
-len++;
-name_copy = malloc(len + 1);
-if (name_copy == NULL)
-{
+free(d->name);
+free(d->owner);
 free(d);
 return (NULL);
 }
-for (i = 0; i <= len; i++)
-name_copy[i] = name[i];
-}
-if (owner != NULL)
-{
-len = 0;
-while (owner[len] != '\0')
-len++;
-owner_copy = malloc(len + 1);
-if (owner_copy == NULL)
-{
-free(name_copy);
-free(d);
-return (NULL);
-}
-for (i = 0; i <= len; i++)
-owner_copy[i] = owner[i];
-}
-d->name = name_copy;
+
 d->age = age;
-d->owner = owner_copy;
+
 return (d);
 }
