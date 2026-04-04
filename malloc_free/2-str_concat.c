d04e7e85e53a08c766df9544164fbbda52d138c8
ghaid999 <gaid.909@gmail.com>
2026-04-04 14:47:53 +0300

    Refactor str_concat for improved readability

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,35 +9,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *new_str;
-    int i, j, len1 = 0, len2 = 0;
+char *new_str;
+int i, j, len1 = 0, len2 = 0;
 
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
 
 
-    new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-    if (new_str == NULL)
-        return (NULL);
+new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+if (new_str == NULL)
+return (NULL);
 
 
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
+for (i = 0; i < len1; i++)
+new_str[i] = s1[i];
 
 
-    for (j = 0; j < len2; j++)
-        new_str[i + j] = s2[j];
+for (j = 0; j < len2; j++)
+new_str[i + j] = s2[j];
 
-    new_str[len1 + len2] = '\0';
+new_str[len1 + len2] = '\0';
 
-    return (new_str);
+return (new_str);
 }
