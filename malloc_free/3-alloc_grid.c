0f1b51d286eeeac39194e6c9d7ac5e608fee3cb9
ghaid999 <gaid.909@gmail.com>
2026-04-04 14:53:14 +0300

    Fix return statements in alloc_grid function

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,11 +13,11 @@ int **grid;
 int i, j;
 
 if (width <= 0 || height <= 0)
-return NULL;
+return (NULL);
 
 grid = malloc(height * sizeof(int *));
 if (grid == NULL)
-return NULL;
+return (NULL);
 
 for (i = 0; i < height; i++)
 {
@@ -27,11 +27,11 @@ if (grid[i] == NULL)
 for (j = 0; j < i; j++)
 free(grid[j]);
 free(grid);
-return NULL;
+return (NULL);
 }
 for (j = 0; j < width; j++)
 grid[i][j] = 0;
 }
 
-return grid;
+return (grid);
 }
