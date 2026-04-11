17717ab6fec5e068233faf8aba33867d2492c524
ghaid999 <gaid.909@gmail.com>
2026-04-11 22:20:53 +0300

    Refactor print_name function documentation and formatting

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,14 +4,14 @@
 /**
  * print_name - prints a name using a function pointer
  * @name: name to print
- * @f: pointer to function that prints the name
+ * @f: pointer to function
  *
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }
