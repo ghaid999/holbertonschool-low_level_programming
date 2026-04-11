720980ab151a92b27b31ba93476c60c7f2a57d65
ghaid999 <gaid.909@gmail.com>
2026-04-11 22:51:46 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -20,28 +20,25 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		if (format[i] == 'c')
-		{
 			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
 		else if (format[i] == 'i')
-		{
 			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
 		else if (format[i] == 'f')
-		{
 			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-		}
 		else if (format[i] == 's')
 		{
 			s = va_arg(args, char *);
 			if (s == NULL)
 				s = "(nil)";
 			printf("%s%s", sep, s);
-			sep = ", ";
 		}
+		else
+		{
+			i++;
+			continue;
+		}
+
+		sep = ", ";
 		i++;
 	}
 
