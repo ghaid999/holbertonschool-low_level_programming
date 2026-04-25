3cefb4b776be8a988be842f1b09505a380d1334b
ghaid999 <gaid.909@gmail.com>
2026-04-25 20:28:51 +0300

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -11,5 +11,5 @@
  * Return: number of bytes printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 #endif
