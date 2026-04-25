b7c1c984e3b55f614ff3478c1d48298f6105a163
ghaid999 <gaid.909@gmail.com>
2026-04-25 20:40:24 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,19 +2,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "main.h"
 #define BUFF_SIZE 1024
-
 /**
  * main - copies file_from to file_to
  * @argc: argument count
- * @argv: arguments
+ * @argv: argument vector
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t r, w, w_total;
+	int fd_from, fd_to, r, w, c_from, c_to;
 	char buffer[BUFF_SIZE];
 
 	if (argc != 3)
@@ -22,14 +20,12 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -37,25 +33,17 @@ int main(int argc, char *argv[])
 		close(fd_from);
 		exit(99);
 	}
-
 	while ((r = read(fd_from, buffer, BUFF_SIZE)) > 0)
 	{
-		w_total = 0;
-
-		while (w_total < r)
+		w = write(fd_to, buffer, r);
+		if (w != r)
 		{
-			w = write(fd_to, buffer + w_total, r - w_total);
-			if (w == -1)
-			{
-				dprintf(2, "Error: Can't write to %s\n", argv[2]);
-				close(fd_from);
-				close(fd_to);
-				exit(99);
-			}
-			w_total += w;
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
 		}
 	}
-
 	if (r == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
@@ -63,18 +51,17 @@ int main(int argc, char *argv[])
 		close(fd_to);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1)
+	c_from = close(fd_from);
+	if (c_from == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
-	if (close(fd_to) == -1)
+	c_to = close(fd_to);
+	if (c_to == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
