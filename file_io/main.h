f4acf06506b3258e06d314c8b91b78f39f39206e
ghaid999 <gaid.909@gmail.com>
2026-04-25 20:26:58 +0300

    Create main.h

---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: name of file
+ * @letters: number of letters to read
+ * Return: number of bytes printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
