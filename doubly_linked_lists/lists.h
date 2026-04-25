8458665f0fdce1ae9246cb4629fdb3744a1f9c7a
ghaid999 <gaid.909@gmail.com>
2026-04-25 19:35:56 +0300

    Create lists.h

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: integer value
+ * @prev: pointer to previous node
+ * @next: pointer to next node
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototype */
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
